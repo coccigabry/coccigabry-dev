9c643c326a9ff2d85222fc9774083c76072c7727
coccigabry <coccigab@gmail.com>
2025-05-13 19:13:00 +0200

    portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,20 @@
-"use client"
+"use client";
 
-import { motion } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
+import { useRef } from "react";
+import {
+  portfolioGetTouch,
+  portfolioGetTouchBtn,
+  portfolioGetTouchRotatingText,
+  portfolioTitle,
+} from "../../../public/content";
+import Link from "next/link";
+import Carousel from "@/components/carousel";
 
 const PortfolioPage = () => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({ target: ref });
+
   return (
     <motion.div
       className="h-full"
@@ -10,7 +22,44 @@ const PortfolioPage = () => {
       animate={{ y: 0 }}
       transition={{ duration: 1 }}
     >
-      <div className="">PortfolioPage</div>
+      <div className="h-[600vh] relative" ref={ref}>
+        {/* TITLE */}
+        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
+          {portfolioTitle}
+        </div>
+        {/* CAROUSEL */}
+        <Carousel scrollYProgress={scrollYProgress} />
+      </div>
+      {/* CONTACT ME */}
+      <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
+        <h1 className="text-8xl">{portfolioGetTouch}</h1>
+        <div className="relative">
+          <motion.svg
+            animate={{ rotate: 360 }}
+            transition={{ duration: 8, ease: "linear", repeat: Infinity }}
+            viewBox="0 0 300 300"
+            className="w-64 h-64 md:w-[500px] md:h-[500px]"
+          >
+            <defs>
+              <path
+                id="circlePath"
+                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+              />
+            </defs>
+            <text fill="#000">
+              <textPath xlinkHref="#circlePath" className="text-4xl">
+                {portfolioGetTouchRotatingText}
+              </textPath>
+            </text>
+          </motion.svg>
+          <Link
+            href="/contact"
+            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
+          >
+            {portfolioGetTouchBtn}
+          </Link>
+        </div>
+      </div>
     </motion.div>
   );
 };
---
+++ src/components/carousel.jsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { motion, useTransform } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { projects } from "../../public/content";
+
+const Carousel = ({ scrollYProgress }) => {
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-75%"]);
+
+  return (
+    <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
+      <motion.div style={{ x }} className="flex">
+        <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-blue-300 to-purple-300" />
+        {projects &&
+          projects.map((project) => {
+            return (
+              <div
+                className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${project.bg}`}
+                key={project.title}
+              >
+                <div className="flex flex-col gap-8 text-white">
+                  <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl text-center">
+                    {project.title}
+                  </h1>
+                  <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] ml-auto mr-auto">
+                    <Image src={project.img} alt={project.alt} fill />
+                  </div>
+                  <p className="w-80 md:w-96 lg:w-[500px] lg:text-lg xl:w-[400px] text-center ml-auto mr-auto">
+                    {project.desc}
+                  </p>
+                  <Link
+                    href={project.link}
+                    target="_blank"
+                    className="flex justify-end"
+                  >
+                    <button className="p-2 text-sm md:p-4 md:text-md lg:p-6 lg:text-lg bg-white text-blue-800 font-bold m-4 border-2 border-black rounded">
+                      See Source Code
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            );
+          })}
+      </motion.div>
+    </div>
+  );
+};
+
+export default Carousel;
