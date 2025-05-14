accb7d04e002e46c2b7504934e50c708d09b4c42
coccigabry <coccigab@gmail.com>
2025-05-14 16:52:27 +0200

    multi-language support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./public/*.{js,ts,jsx,tsx,mdx}",
+      "./public/*.{json,js,ts,jsx,tsx,mdx}",
       "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
       "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
       "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
